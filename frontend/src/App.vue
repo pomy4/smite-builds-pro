69ff10725dec7da3e1d10bebbd68bd0fe8d8340a
NAZADOTH <108572837+NAZADOTH@users.noreply.github.com>
2021-09-14 22:13:53 +0200

    Throw in Bulma, Eslint and various configs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <div>
-    <label for="players">Choose a player:</label>
-    <select v-on:change="choose_player" id="players">
-      <option v-for="player in players" :value="player">
-        {{ player }}
-      </option>
-    </select>
-    <ul>
-      <li v-for="build in builds">
-        {{ build.player }} {{ build.role }} {{ build.god }}
-      </li>
-    </ul>
+  <div class="section">
+    <div class="container">
+      <div class="tabs is-centered is-medium is-boxed">
+        <ul>
+          <li class="is-active"><a>Basic search</a></li>
+          <li><a>Advanced search</a></li>
+        </ul>
+      </div>
+      <div class="has-text-centered">
+        <label for="roles">God: </label>
+        <select class="select" id="gods">
+          <option v-for="god in god1s" v-bind:key="god">
+            {{ god }}
+          </option>
+        </select>
+        <label for="roles">Role: </label>
+        <select class="select" id="roles">
+          <option v-for="role in roles" v-bind:key="role">
+            {{ role }}
+          </option>
+        </select>
+        <button class="button">Find builds</button>
+      </div>
+      <ul class="has-text-centered">
+        <li v-for="build in builds" v-bind:key="build.id">
+          {{ build.season }} {{ build.league }} {{ build.phase }}
+          {{ build.date }} {{ build.role }}
+          {{ build.game_i }} {{ build.win }} {{ build.game_length }}
+          {{ build.kills }} {{ build.deaths }} {{ build.assists }}
+          {{ build.team1 }} {{ build.player1 }} {{ build.god1 }}
+          {{ build.team2 }} {{ build.player2 }} {{ build.god2 }}
+          {{ build.relic1 }} {{ build.relic2 }}
+          {{ build.item1 }} {{ build.item2 }} {{build.item3 }}
+          {{ build.item4 }} {{ build.item5 }} {{build.item6 }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -19,7 +44,8 @@
     data() {
       return {
         backend: import.meta.env.PROD ? 'https://gebgebgeb.pythonanywhere.com' : 'http://localhost:8080',
-        players: [],
+        god1s: ['Loading ...'],
+        roles: ['Loading ...'],
         builds: []
       }
     },
@@ -31,16 +57,26 @@
         .then(json => this.builds = json)
         .catch(error => console.log(error))
       },
-      populate_select() {
-        fetch(`${this.backend}/players`)
+      get_select_options() {
+        fetch(`${this.backend}/select_options`)
         .then(response => response.json())
-        .then(json => this.players = json)
+        .then(json => {
+          this.god1s = json['god1s']
+          this.roles = json['roles']
+          })
+        .catch(error => console.log(error))
+      },
+      get_builds() {
+        fetch(`${this.backend}/builds`)
+        .then(response => response.json())
+        .then(json => this.builds = json)
         .catch(error => console.log(error))
       }
     },
 
     mounted() {
-      this.populate_select()
+      this.get_select_options()
+      this.get_builds()
     }
   }
 </script>
