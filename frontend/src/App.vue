23ec99065b2be3fc528732a0368a36617f9bfd00
NAZADOTH <108572837+NAZADOTH@users.noreply.github.com>
2021-09-24 23:09:14 +0200

    Add url into the lifecycle of the app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -42,14 +42,18 @@
     },
     data() {
       return {
-        select_god1s: undefined,
-        select_roles: undefined,
-        filters_god1s: [],
-        filters_roles: [],
         builds: [],
         page: 1,
         watch_for_intersections: false,
         watch_for_intersections_timeout: undefined,
+        selects: {},
+        filters: {},
+        translations: {'god1s': 'God', 'roles': 'Role'}, // This will be used for printing.
+        // This will be used in the basic view.
+        select_god1: undefined,
+        select_role: undefined,
+        filter_god1: [],
+        filter_role: [],
       }
     },
     methods: {
@@ -63,13 +67,9 @@
       button_clicked() {
         clearTimeout(this.watch_for_intersections_timeout)
         this.watch_for_intersections = false
-        this.update_filters()
+        this.selects_to_filters_and_client_url()
         this.reset_builds()
         this.get_builds()
-      },
-      update_filters() {
-        this.filters_god1s = this.select_god1s.getValue()
-        this.filters_roles = this.select_roles.getValue()
       },
       reset_builds() {
         this.builds = []
@@ -78,13 +78,7 @@
       async get_builds() {
         let bottom_of_page = document.getElementById('bottom-of-page')
         bottom_of_page.textContent = 'Loading builds ...'
-        let url = `/api/builds?page=${this.page}`
-        for (let god1 of this.filters_god1s) {
-          url += `&god1=${god1}`
-        }
-        for (let role of this.filters_roles) {
-          url += `&role=${role}`
-        }
+        let url = `/api/builds${this.filters_to_server_url_fragment()}`
         let response = await fetch(url)
         if (! response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`)
@@ -141,14 +135,58 @@
       start_watching_in_the_future() {
         this.watch_for_intersections_timeout = setTimeout(()=>
           this.watch_for_intersections=true, 50)
+      },
+      selects_to_filters_and_client_url() {
+        let url_fragment = `?view=basic`
+        for (const [keys, select] of Object.entries(this.selects)) {
+          const vals = select.getValue()
+          this.filters[keys] = vals
+          for (const val of vals) {
+            url_fragment += `&${keys.slice(0, -1)}=${val}`
+          }
+        }
+        let url_object = new URL(url_fragment, window.location.origin)
+        history.pushState(undefined, '' , url_object.href)
+      },
+      client_url_to_filters_and_selects() {
+        this.filters = {}
+        for (const select of Object.values(this.selects)) {
+          select.clear()
+        }
+        let search_params = new URL(window.location.href).searchParams
+        for (const key of search_params.keys()) {
+          console.log(key)
+          if (key == 'view') {
+            continue // TODO
+          }
+          const keys = `${key}s`
+          const vals = search_params.getAll(key)
+          this.filters[keys] = vals
+          for (const val of vals) {
+            this.selects[keys].addItem(val)
+          }
+        }
+        // TODO print selected filters
+      },
+      filters_to_server_url_fragment() {
+        let url = `?page=${this.page}`
+        for (const [key, vals] of Object.entries(this.filters)) {
+          for (const val of vals) {
+            url += `&${key.slice(0, -1)}=${val}`
+          }
+        }
+        return url
       }
     },
     async mounted() {
-      this.select_god1s = this.create_select('god1s')
-      this.select_roles = this.create_select('roles')
       let options = await this.get_select_options()
-      this.update_select(this.select_god1s, options['god1s'])
-      this.update_select(this.select_roles, options['roles'])
+      // Create Tom-selects.
+      for (const node of document.querySelectorAll("select")) {
+        const id = node.id
+        this.selects[id] = this.create_select(id)
+        this.update_select(this.selects[id], options[id])
+      }
+      // Prepare pagination.
       let observer = new IntersectionObserver(()=> {
         if (this.watch_for_intersections) {
           this.watch_for_intersections = false
@@ -156,6 +194,17 @@
         }
       })
       observer.observe(document.getElementById('bottom-of-page'))
+      // Prepare navigation.
+      window.addEventListener('popstate', () => {
+        clearTimeout(this.watch_for_intersections_timeout)
+        this.watch_for_intersections = false
+        // Same as button_clicked except for this line.
+        this.client_url_to_filters_and_selects()
+        this.reset_builds()
+        this.get_builds()
+      })
+
+      this.client_url_to_filters_and_selects()
       this.get_builds()
     }
   }
