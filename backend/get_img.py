04a04373493d2a7601a2ffdceb2fef4228f1791b
NAZADOTH <108572837+NAZADOTH@users.noreply.github.com>
2022-09-26 14:07:37 +0200

    Add a small script for getting item images saved in DB

---
+++ backend/get_img.py
@@ -0,0 +1,17 @@
+import base64
+import sys
+
+from models import Item, db
+
+
+def main() -> None:
+    pk = int(sys.argv[1])
+    with db:
+        item = Item.get_by_id(pk)
+    image_data = base64.b64decode(item.image_data)
+    with open(f"{pk}.jpg", "wb") as f:
+        f.write(image_data)
+
+
+if __name__ == "__main__":
+    main()
