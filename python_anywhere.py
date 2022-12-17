eb9c4cbee146647bf61efc3d3dec447e6e455604
NAZADOTH <108572837+NAZADOTH@users.noreply.github.com>
2022-12-17 14:29:47 +0100

    Add python_anywhere.py

---
+++ python_anywhere.py
@@ -0,0 +1,18 @@
+"""
+This file is only used on PythonAnywhere, but it is kept in the repo so that it can
+be version controlled. The only requirement is that the "application" variable is set
+to a WSGI handler, so the code that is in the actual PythonAnywhere configuration is
+"from <this file> import application" (and some sys.path manipulation).
+"""
+
+import os
+import time
+
+import be.backend
+import shared
+
+os.environ["TZ"] = "Europe/Prague"
+time.tzset()
+
+shared.load_default_dot_env()
+application = be.backend.app
