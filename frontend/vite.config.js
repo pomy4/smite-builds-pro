6521a9a2d70c5e10333a95411846e6d1eff47afe
pomy4 <108572837+pomy4@users.noreply.github.com>
2026-04-16 22:40:02 +0200

    rollup-plugin-gzip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { promisify } from "node:util";
+import { brotliCompress } from "node:zlib";
+import gzipPlugin from "rollup-plugin-gzip";
+
+const brotliPromise = promisify(brotliCompress);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +12,14 @@ export default defineConfig({
     outDir: '../static',
     emptyOutDir: true,
   },
-  plugins: [vue()],
+  plugins: [
+    // https://github.com/kryops/rollup-plugin-gzip?tab=readme-ov-file#brotli-compression
+    gzipPlugin({
+        customCompression: (content) => brotliPromise(Buffer.from(content)),
+        fileName: ".br",
+    }),
+    vue()
+  ],
   server: {
     open: '/',
     proxy: {
